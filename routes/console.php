629c361755959d380785c5431d7e55f5cf44520f
Haikal <mnurmahaikal106@gmail.com>
2026-08-06 15:00:47 +0700

    feat: schedule auto carry-forward cuti tahunan (Task 33)

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Task 33: Schedule Auto Carry-forward Cuti setiap awal tahun
+Schedule::command('leave:carry-forward')->yearly();
