70e88290a3e18d3b4830348608d346a84c1e8b04
rmysf <mramayusuf2@gmail.com>
2026-08-07 14:38:21 +0700

    feat: update fitur pada modul HR

--- routes/web.php
+++ routes/web.php
@@ -53,3 +53,9 @@ Route::middleware('auth')->group(function () {
     });
 
 });
+
+require __DIR__.'/admin.php';
+require __DIR__.'/hr.php';
+require __DIR__.'/finance.php';
+require __DIR__.'/supervisor.php';
+
